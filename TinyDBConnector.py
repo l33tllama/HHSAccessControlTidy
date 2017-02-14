cb420d4b9d74a39b8dc312e7e1f9da4db760fa16
Leo Febey <overclockedamiga@gmail.com>
2017-02-14 13:42:43 +1100

    Prev commit pt 2

--- TinyDBConnector.py
+++ TinyDBConnector.py
@@ -14,13 +14,14 @@ class TinyDBConnector:
             for custom_field in rfid_contact['custom_fields']:
                 if custom_field['title'] == 'RFID Tag':
                     if custom_field['value'] == str(rfid):
-                         rfid_match = True
-                         rfid_contact_found = rfid_contact
-                    print "Tag id: " + custom_field['value'] + " name: " + rfid_contact['first_name'] + " " + rfid_contact['last_name'] + " Tag state: " + rfid_contact['membership_state']
+                        print ("RFID Match: " + str(custom_field['value']))
+                        rfid_match = True
+                        rfid_contact_found = rfid_contact
+                    #print "Tag id: " + custom_field['value'] + " name: " + rfid_contact['first_name'] + " " + rfid_contact['last_name'] + " Tag state: " + rfid_contact['membership_state']
 
         if rfid_match is True:
             if rfid_contact_found['membership_state'] == 'activated':
-                return (rfid_contact, True)
-            else: return (rfid_contact, False)
+                return (rfid_contact_found, True)
+            else: return (rfid_contact_found, False)
         else:
             return (None, False)
